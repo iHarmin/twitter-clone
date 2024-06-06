92d329469e698e2862c23686fbcf3b2ea7a4f1f5
Simon <98567864+Simon-Losier@users.noreply.github.com>
2024-06-06 09:23:35 -0300

    Added springboot boilerplate

---
+++ springboot/twitter-2/src/test/java/com/group06/twitter2/Twitter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group06.twitter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twitter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
