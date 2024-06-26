0cadfd4c0f03c45c418aa1287e107b60ce82a0cb
Moses Tong <moses.tong@dal.ca>
2024-06-26 12:22:07 -0300

    feat: created navigation bar and route to nav bar in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProfilePage from './ProfilePage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <div>
         <Routes>
           <Route path="/profile" element={<ProfilePage />} />
