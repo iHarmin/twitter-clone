27c42403482323f7ba9debc8376a6c1f8496f114
Moses Tong <moses.tong@dal.ca>
2024-06-27 00:29:13 -0300

    feat: added change status function in profile page

--- frontend/src/ProfilePage.tsx
+++ frontend/src/ProfilePage.tsx
@@ -8,15 +8,17 @@ function ProfilePage() {
     lastName: '',
     userName: '',
     email: '',
-    interests: ''
+    interests: '',
+    status: 'Online 🟢' // default status
   });
 
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [interests, setInterests] = useState('');
+  const [status, setStatus] = useState('Online 🟢');
 
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handleDetailsSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     setFormData({
@@ -24,7 +26,7 @@ function ProfilePage() {
       firstName: firstName,
       lastName: lastName,
       email: email,
-      interests: interests
+      interests: interests,
     });
 
 
@@ -47,19 +49,45 @@ function ProfilePage() {
     }
   };
 
+  const handleStatusChange = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setFormData({
+      ...formData,
+      status: status,
+    });
+
+    try {
+      const serverResponse = await fetch('http://localhost:8080/api/users/save_status', {
+        method: "POST",
+        headers: {
+          "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const result = await serverResponse.json();
+      console.log('Success:', result);
+      alert('Status Changes Saved!');
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while saving your status changes.');
+    }
+  }
+
   // Form for entering name, email, and interests
   // Username cannot be changed
   return (
     <>
       <div>
-        <h2>User Profile</h2>
+        <h2>My Profile</h2>
         <p>{formData.firstName} {formData.lastName}</p>
         <p>{formData.userName}</p>
         <p>{formData.email}</p>
         <p>{formData.interests}</p>
+        <p>{formData.status}</p>
       </div>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleDetailsSubmit}>
         <label>
           First Name:
           <input type="text" value={firstName}
@@ -85,6 +113,18 @@ function ProfilePage() {
                  className="form-control"/>
         </label>
         <input type="submit" value="Submit" className="btn btn-primary"/>
+      </form>
+
+      <form onSubmit={handleStatusChange}>
+        <label>
+          Status:
+          <select value={status} onChange={e => setStatus(e.target.value)} className="form-control">
+            <option value="Online 🟢">Online</option>
+              <option value="Offline ⚪">Offline</option>
+            <option value="Busy 🔴">Busy</option>
+          </select>
+        </label>
+        <input type="submit" value="Change Status" className="btn btn-primary"/>
       </form>
     </>
   );
