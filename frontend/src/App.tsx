0ae683d24f4bcd2c64e61f6a61e29e7590809456
Moses Tong <moses.tong@dal.ca>
2024-06-27 20:17:11 -0300

    feat: added forgot password route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from './NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from "./Login.tsx";
 import Signup from "./Signup.tsx";
+import ForgotPassword from "./ForgotPassword.tsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/profile/:username" element={<ProfilePage />} />
         </Routes>
       </div>
