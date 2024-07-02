ad2e4875b78738ed3373303525990fdcf2788887
Moses Tong <moses.tong@dal.ca>
2024-07-01 22:17:48 -0300

    feat: added page reload notification

--- frontend/src/Logout.tsx
+++ frontend/src/Logout.tsx
@@ -12,7 +12,8 @@ const Logout = () => {
     Cookies.remove('username');
     setIsLoggedIn(false);
     navigate('/'); // Redirect to the home page or login page
-    console.log('Logged out, dispatching Cookie Monster to eat your cookies! Nom nom nom!'); 
+    console.log('Logged out, dispatching Cookie Monster to eat your cookies! Nom nom nom!');
+    alert('You have been logged out! Please reload page');
   };
 
   return (
