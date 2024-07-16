edd4227f7dd4ad1b36b16ee4d2fbf7e18db1e787
Simon <98567864+Simon-Losier@users.noreply.github.com>
2024-07-16 09:14:28 -0300

    Added test classes

---
+++ springboot/twitter-2/src/test/java/com/group06/twitter2/FriendShipServiceTest.java
@@ -0,0 +1,7 @@
+package com.group06.twitter2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FriendShipServiceTest {
+}
---
+++ springboot/twitter-2/src/test/java/com/group06/twitter2/PostServiceTest.java
@@ -0,0 +1,8 @@
+package com.group06.twitter2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostServiceTest {
+
+}
