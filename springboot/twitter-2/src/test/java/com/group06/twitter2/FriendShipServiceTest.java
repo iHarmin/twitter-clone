14cb1c56c96af013c7ba61282a841b55a8586416
Simon <98567864+Simon-Losier@users.noreply.github.com>
2024-07-16 15:08:27 -0300

    Added boilerplate for friendship test

--- springboot/twitter-2/src/test/java/com/group06/twitter2/FriendShipServiceTest.java
+++ springboot/twitter-2/src/test/java/com/group06/twitter2/FriendShipServiceTest.java
@@ -1,7 +1,23 @@
 package com.group06.twitter2;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.group06.twitter2.repository.FriendshipRepository;
+import com.group06.twitter2.service.Implementations.FriendshipServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class FriendShipServiceTest {
+    @InjectMocks
+    FriendshipServiceImpl friendshipService;
+
+    @Mock
+    FriendshipRepository friendshipRepository;
+
+    @Test
+    public void sampleTest() {
+        assert(true);
+    }
 }
