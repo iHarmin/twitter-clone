5038da390f3c08fd0aa9deb1e572f482dad722d3
Harmin Patel <i_harmin@Harmins-MacBook-Pro.local>
2024-07-23 12:28:16 -0300

    feat: Added required constants for removePerson feature

--- frontend/src/removePerson.tsx
+++ frontend/src/removePerson.tsx
@@ -3,7 +3,8 @@ import React, {useContext, useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RemovePerson: React.FC = () => {
-
+    const [userEmail, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 }
 
 export default RemovePerson;
