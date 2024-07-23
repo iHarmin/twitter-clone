73940ff056f4184668beff0f1fecd46000de0a99
Harmin Patel <i_harmin@Harmins-MacBook-Pro.local>
2024-07-23 12:25:37 -0300

    chore: Added new file removePerson.tsx in frontend

---
+++ frontend/src/removePerson.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import React, {useContext, useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const RemovePerson: React.FC = () => {
+
+}
+
+export default RemovePerson;
