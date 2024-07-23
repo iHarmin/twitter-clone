c083608a52364687e43bda81c73e5a4d26f4b247
Harmin Patel <i_harmin@Harmins-MacBook-Pro.local>
2024-07-23 12:41:56 -0300

    feat: Added serverResponse to connect frontend with backend for removePerson.tsx

--- frontend/src/removePerson.tsx
+++ frontend/src/removePerson.tsx
@@ -18,6 +18,27 @@ const RemovePerson: React.FC = () => {
         };
 
         console.log(user);
+
+        try {
+            const serverResponse = await fetch(`http://localhost:8080/api/users/removeUserByAdmin`, {
+              method: "POST",
+              headers: {
+                "Content-Type": 'application/json',
+              },
+              body: JSON.stringify(user),
+            }) 
+      
+            if (serverResponse.ok) {
+              const result = await serverResponse.text();
+              console.log('Success:', result);
+              alert("User deleted successfully!");
+            } else {
+              throw new Error('Server response was not OK!');
+            }
+          } catch (error) {
+            console.error(error);
+            alert('An error occurred while adding new person.');
+          }
     };
     
 }
