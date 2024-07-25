b7e126e65e723cbfee4a7682f8700f599084bff4
ChrisSimoneau <ch258217@dal.ca>
2024-07-24 23:04:57 -0300

    feat: added private groups

--- frontend/src/CreateGroup.tsx
+++ frontend/src/CreateGroup.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-
 import { useNavigate } from 'react-router-dom';
 
 const CreateGroup: React.FC = () => {
   const [groupName, setGroupName] = useState("");
   const [isPublic, setIsPublic] = useState(true);
+  const [isPrivate, setIsPrivate] = useState(false);
 
   const navigate = useNavigate();
 
@@ -12,19 +12,21 @@ const CreateGroup: React.FC = () => {
     setGroupName(e.target.value);
   };
 
-  const handleToggleChange = () => {
+  const handlePublicToggleChange = () => {
     setIsPublic(!isPublic);
+  };
+
+  const handlePrivateToggleChange = () => {
+    setIsPrivate(!isPrivate);
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // just for testing.
     console.log("Group Name:", groupName);
     console.log("Is Public:", isPublic);
+    console.log("Is Private:", isPrivate);
 
-    // send fetch request to backend, expect a Group response
-    const newGroup = { groupName, isPublic }; 
-    // TODO: Probably make a nice TypeScript model to match the backend model instead of winging it with vanilla JS
+    const newGroup = { groupName, isPublic, isPrivate }; 
     const response = await fetch(
       "http://localhost:8080/api/groups/createGroup",
       {
@@ -37,11 +39,9 @@ const CreateGroup: React.FC = () => {
     );
 
     if (!response.ok) {
-      // response was error, pop an alert maybe?
+      alert("Error creating group");
     } else {
-      console.log("we won"); // TODO: Probably modify this stuff
       const group = await response.json();
-      // navigate to group detail after successful creation (add group.id to page)
       navigate(`/group/${group.id}`);
     }
   };
@@ -65,7 +65,16 @@ const CreateGroup: React.FC = () => {
             type="checkbox"
             id="isPublic"
             checked={isPublic}
-            onChange={handleToggleChange}
+            onChange={handlePublicToggleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="isPrivate">Private:</label>
+          <input
+            type="checkbox"
+            id="isPrivate"
+            checked={isPrivate}
+            onChange={handlePrivateToggleChange}
           />
         </div>
         <button type="submit">Create Group</button>
